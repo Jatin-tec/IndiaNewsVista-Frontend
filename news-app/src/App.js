b9f0d21cfef6a045d5fd26ed7fd53c1e1538eb86
urvashi <urvashikshatriya9@gmail.com>
2023-10-04 08:24:42 +0530

    New Commit

---
+++ news-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import NavTop from './components/NavTop';
+import MainSlider from './components/MainSlider';
+function App() {
+  return (
+    <>
+    <NavTop/>
+    <NavBar/>
+    <MainSlider/>
+    </>
+  );
+}
+
+export default App;
